366e2a2859cec21447170492826d06a086ac90c7
Gantumur <gantumur0307@gmail.com>
2021-04-01 19:34:48 +0800

    2 toglogchiin eeljiig solih cod bichlee

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // toglogchiin eeljiig hadgalah huwisagch 1-player 0, 2-player 1 gej temdeglii
-var activePlayer=1;
+var activePlayer=0;
 
 // toglogchiin tsugluulsan onoog hadgalah huwisagch
 var scores=[0, 0];
@@ -21,14 +21,52 @@ document.getElementById('current-1').textContent=0;
 var diceDom=document.querySelector(".dice");
 document.querySelector('.dice').style.display="none";
 
+
+//shoog shideh event lister
 document.querySelector(".btn-roll").addEventListener("click", function (){
+    //1-6 dotorh sanamsargui toog gargaj awna
     var diceNumber=Math.floor(Math.random()*6)+1;
-    document.querySelector('.dice').style.display="block";
-    var diceDom=document.querySelector(".dice");
+
+    //shoonii zurgiig web deer gargaj irne
+    diceDom.style.display='block';
+    // document.querySelector('.dice').style.display="block";
+    // var diceDom=document.querySelector(".dice");
+
+    //buusan sanamsargui toond hargalzah zurgiig gargaj ugnu
     document.querySelector('.dice').src='dice-'+diceNumber+'.png';
-
     // alert('shoo buulaa: ' +diceNumber)
+
+    //buusan too ni 1s ylgaatai bol idevhitei toglogchiin eeljiin onoog nemegduulne
+    if(diceNumber !==1){
+        //1s ylgaatai too buula.Buusan toog toglogchid nemj ugnu
+        roundScore=roundScore+diceNumber;
+        document.getElementById('current-'+ activePlayer).textContent=roundScore;
+    }else{
+        // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu
+        // ENE toglogchiin eeljindee bussan onoog 0 bolgono
+        roundScore=0;
+        document.getElementById('current-'+ activePlayer).textContent=0;
+
+        //herev idevhitei togloch ni 0 baiwal idewhtei toglogchiig 1 bolgo.
+        //ugui bol 0 bolgo
+        activePlayer===0 ? (activePlayer=1) : (activePlayer=0);
+        // if(activePlayer===0){
+        //     activePlayer=1;
+        // }else{
+        //     activePlayer=0;
+        // }
+
+        
+        //ulaan tsegiig shiljvvleh
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //shoog tur alga bolgono
+        diceDom.style.display='none';
+
+        //onoog hadgalah
+        doc
+
+        
+    }
 });
-
-
-{/* <button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button> */}
