cb7fc0f30f04723f421d3792076c41a34458bd33
Gantumur <gantumur0307@gmail.com>
2021-04-01 16:39:13 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,19 +8,27 @@ var scores=[0, 0];
 var roundScore=0;
 
 // shoonii ali talaar buussaniig hadgalah huwisagch heregtea, 1-6 gesen utgiig ene huwisagchid sanamsarguigeer uusgej ugnu
-var dice=Math.floor(Math.random()*6)+1;
-
-//<div class="player-score" id="score-0">43</div>
-// window.document.querySelector('#score-0').textContent=dice;
-
-// document.querySelector('#score-1').innerHTML="<em>Yes<em>"
+var diceNumber=Math.floor(Math.random()*6)+1;
 
 
-document.querySelector('#score-0').textContent=0;
-document.querySelector('#score-1').textContent=0;
-document.querySelector('#current-0').textContent=0;
-document.querySelector('#current-1').textContent=0;
 
+//program ehlehed belgie
+document.getElementById('score-0').textContent=0;
+document.getElementById('score-1').textContent=0;
+document.getElementById('current-0').textContent=0;
+document.getElementById('current-1').textContent=0;
+
+var diceDom=document.querySelector(".dice");
 document.querySelector('.dice').style.display="none";
 
-console.log('shoo; '+dice );
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    var diceNumber=Math.floor(Math.random()*6)+1;
+    document.querySelector('.dice').style.display="block";
+    var diceDom=document.querySelector(".dice");
+    document.querySelector('.dice').src='dice-'+diceNumber+'.png';
+
+    // alert('shoo buulaa: ' +diceNumber)
+});
+
+
+{/* <button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button> */}
